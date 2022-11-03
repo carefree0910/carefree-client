efc7582a74c89969d27420f6b59726c9fa1a7a8d
carefree0910 <syameimaru.saki@gmail.com>
2022-11-03 19:56:09 +0800

    📦️Upgraded `VERSION`

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = "0.1.2"
+VERSION = "0.1.3"
 DESCRIPTION = "Client framework based on FastAPI"
 with open("README.md", encoding="utf-8") as f:
     LONG_DESCRIPTION = f.read()
